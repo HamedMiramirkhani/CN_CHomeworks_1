257bf3a98d2ce3eed71d14d6ca5685974882c775
Mostafa <mostafa.ebrahimi2002@gmail.com>
2023-03-12 23:18:34 +0330

    server/src/Date: check range of day,month,year  #2

--- server/src/Date.cpp
+++ server/src/Date.cpp
@@ -18,5 +18,15 @@ bool Date::isValidDateFormat(const std::string &dateStr)
 {
     // Regular expression to match a date in the format DD/MM/YYYY
     std::regex dateRegex("\\d{2}/\\d{2}/\\d{4}");
-    return std::regex_match(dateStr, dateRegex);
+    if(!std::regex_match(dateStr, dateRegex))
+        return false;
+    
+    // Check if the range of the date is valid
+    int day = std::stoi(dateStr.substr(0, 2));
+    int month = std::stoi(dateStr.substr(3, 2));
+    int year = std::stoi(dateStr.substr(6, 4));
+    if (day < 1 || day > 31 || month < 1 || month > 12 || year < 0)
+        return false;
+
+    return true;
 }
